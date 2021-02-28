e1fe3765f533bf890eab3de80050be8264001942
Michael Bentley <michaelbentley95@gmail.com>
2021-02-28 11:08:48 -0500

    Implemented MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://testuser:<PASSWORD>@cluster0.opxck.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(res=>{
+      console.log("Connected to Mongo");
+    })
+  .catch(err=>{
+      console.log("Connection to Mongo Failed")
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,16 +25,27 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  res.status(201).json({message: "post added"});
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then(createdPost =>{
+    res.status(201).json({message: "post added", data: createdPost._id});
+  });
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    { id: '1', title: 'Server Works', content: 'congrats. The server works.'},
-    { id: '2', title: 'Server Works Still', content: 'congrats. The server still works.'}
-  ];
-  res.status(200).json({message: 'posts retrieved successfully', data: posts});
+app.get('/api/posts', (req, res, next) => {
+  Post.find().then(docs=>{
+    res.status(200).json({message: 'posts retrieved successfully', data: docs});
+  });
 });
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({message: "Post deleted!"});
+  });
+});
+
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from './post.model';
 import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,21 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts(){
-    this.http.get<{message: string, data: Post[]}>('http://localhost:3000/api/posts').subscribe((res)=>{
-      this.posts = res.data;
-      this.postsUpdated.next([...this.posts]);
+    this.http
+    .get<{message: string, data: any}>('http://localhost:3000/api/posts')
+    //All of this just to convert _id to id
+    .pipe(map((postData) => {
+      return postData.data.map(post => {
+        return {
+          title: post.title,
+          content: post.content,
+          id: post._id
+        }
+      })
+    }))
+    .subscribe(mappedPosts=>{
+      this.posts = mappedPosts;
+      this.postsUpdated.next([...this.posts]); //Emit a copy of the posts list
     });
   }
 
@@ -25,9 +38,17 @@ export class PostsService {
 
   addPost(title: string, content: string){
     const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post).subscribe((res)=>{
-      console.log(res.message);
+    this.http.post<{message: string, data: string}>('http://localhost:3000/api/posts', post).subscribe((res)=>{
+      post.id = res.data;
       this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
+  }
+
+  deletePost(postId: string){
+    this.http.delete('http://localhost:3000/api/posts/' + postId).subscribe(()=>{
+      const updatedPosts = this.posts.filter(post => post.id != postId);
+      this.posts = updatedPosts;
       this.postsUpdated.next([...this.posts]);
     });
   }
